5a9e064fc591fd671ce3b127265242436f33567b
Ian Garcia <iangarcia@ciencias.unam.mx>
2023-07-27 21:05:22 -0600

    adding division

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,10 @@ def suma(a,b):
     return a+b
 def resta(a,b):
     return a-b
+def division(a,b):
+    if b==0:
+        return "no es posible dividir entre 0"
+    return a//b
 print("Suma: ",suma(a,b))
 print("Resta: ", resta(a,b))
+print("Division", division(a,b))
